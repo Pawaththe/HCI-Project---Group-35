edd6add0bc6394fe98f7029634c4ba9c2608421c
Suresh Wickramarachchi <10898704@students.plymouth.ac.uk>
2025-05-06 20:20:23 +0530

    Adding updates for furniture

--- src/furniture/management/system/model/Furniture.java
+++ src/furniture/management/system/model/Furniture.java
@@ -1,49 +1,52 @@
 package furniture.management.system.model;
+import com.jogamp.opengl.*;
 
-import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLEventListener;
 
 public abstract class Furniture implements GLEventListener {
-    public Furniture() {
-    }
 
     public void display(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
-        gl.glClear(16640);
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        GL2 gl = drawable.getGL().getGL2();
+        final GL2 gl = drawable.getGL().getGL2();
         if (height <= 0) {
             height = 1;
         }
-
-        float h = (float)width / (float)height;
+        final float h = (float) width / (float) height;
         gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(5889);
+        gl.glMatrixMode(GL2.GL_PROJECTION);
         gl.glLoadIdentity();
-        float fovy = 45.0F;
+
+        float fovy = 45.0f;
         float aspect = h;
-        float zNear = 1.0F;
-        float zFar = 20.0F;
-        gl.glFrustum((double)(-aspect) * Math.tan(Math.toRadians((double)fovy) / 2.0), (double)aspect * Math.tan(Math.toRadians((double)fovy) / 2.0), -Math.tan(Math.toRadians((double)fovy) / 2.0), Math.tan(Math.toRadians((double)fovy) / 2.0), (double)zNear, (double)zFar);
-        gl.glMatrixMode(5888);
+        float zNear = 1.0f;
+        float zFar = 20.0f;
+
+        gl.glFrustum(-aspect * Math.tan(Math.toRadians(fovy) / 2.0),
+                aspect * Math.tan(Math.toRadians(fovy) / 2.0),
+                -Math.tan(Math.toRadians(fovy) / 2.0),
+                Math.tan(Math.toRadians(fovy) / 2.0),
+                zNear, zFar);
+
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
 
     public void init(GLAutoDrawable drawable) {
-        GL2 gl = drawable.getGL().getGL2();
-        gl.glShadeModel(7424);
-        gl.glEnable(2929);
-        gl.glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
-        gl.glClearDepth(1.0);
-        gl.glEnable(2896);
-        gl.glEnable(16384);
-        gl.glEnable(2903);
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glShadeModel(GL2.GL_FLAT);
+        gl.glEnable(GL2.GL_DEPTH_TEST);
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        gl.glClearDepth(1.0f);
+        gl.glEnable(GL2.GL_LIGHTING);
+        gl.glEnable(GL2.GL_LIGHT0);
+        gl.glEnable(GL2.GL_COLOR_MATERIAL);
     }
 
     public void dispose(GLAutoDrawable drawable) {
     }
+
 }
