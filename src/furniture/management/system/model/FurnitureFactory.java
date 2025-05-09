1cc99058eed1df5f7fa8f6daa0bbec26ccd07c87
Devnith Tissera <10898683@students.plymouth.ac.uk>
2025-05-09 14:42:17 +0530

    adding new furniture

--- src/furniture/management/system/model/FurnitureFactory.java
+++ src/furniture/management/system/model/FurnitureFactory.java
@@ -1,8 +1,8 @@
-import furniture.management.system.model.Furniture;
+package furniture.management.system.model;
+
+import furniture.management.system.model.*;
 
 public class FurnitureFactory {
-    public FurnitureFactory() {
-    }
 
     public static Furniture create(String type) {
         switch (type) {
@@ -10,6 +10,20 @@ public class FurnitureFactory {
             case "CoffeeTable2":
             case "CoffeeTable3":
                 return new CoffeeTableManager(type);
+            case "ComputerDesk":
+                return new ComputerDeskManager();
+            case "DiningChair":
+                return new DiningChairManager();
+            case "DiningTable":
+                return new DiningTableManager();
+            case "PantryCupboard":
+                return new PantryCupboardManager();
+            case "TvStand":
+                return new TvStandManager();
+            case "Wardrobe":
+                return new WardrobeManager();
+            case "Bed":
+                return new BedManager();
             default:
                 throw new IllegalArgumentException("Unknown furniture: " + type);
         }
@@ -21,6 +35,20 @@ public class FurnitureFactory {
             case "CoffeeTable2":
             case "CoffeeTable3":
                 return new CoffeeTableManager.CoffeeTable2D(type);
+            case "ComputerDesk":
+                return new ComputerDeskManager.ComputerDesk2D();
+            case "DiningChair":
+                return new DiningChairManager.DiningChair2D();
+            case "DiningTable":
+                return new DiningTableManager.DiningTable2D();
+            case "PantryCupboard":
+                return new PantryCupboardManager.PantryCupboard2D();
+            case "TvStand":
+                return new TvStandManager.TvStand2D();
+            case "Wardrobe":
+                return new WardrobeManager.Wardrobe2D();
+            case "Bed":
+                return new BedManager.Bed2D();
             default:
                 throw new IllegalArgumentException("Unknown 2D furniture: " + type);
         }
